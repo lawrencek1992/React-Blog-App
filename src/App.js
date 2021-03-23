16ee2af79353ff0a57835f8cd9d014dc6c46f135
Kelly <lawrencek1992@gmail.com>
2021-03-22 20:48:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
-import Posts from "./components/Posts";
-import Post from "./components/Post";
-import NotFound from "./components/NotFound";
-import PostForm from "./components/PostForm";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import Header from "./components/Header";
+import Posts from "./components/Posts";
+import Post from "./components/Post";
+import PostForm from "./components/PostForm";
+import Message from "./components/Message";
+import NotFound from "./components/NotFound";
 
 import "./App.css";
 
 const App = (props) => {
   const [posts, setPosts] = useState([]);
-  
+  const [message, setMessage] = useState(null);
+
+  const setFlashMessage = (message) => {
+    setMessage(message);
+    setTimeout(() => {
+      setMessage(null);
+    }, 1600);
+  };
+
   const addNewPost = (post) => {
     post.id = posts.length + 1;
     post.slug = encodeURIComponent(
       post.title.toLowerCase().split(" ").join("-")
     );
     setPosts([...posts, post]);
+    setFlashMessage(`saved`);
   };
-  
+
   return (
     <Router>
       <div className="App">
         <Header />
+        {message && <Message type={message} />}
         <Switch>
           <Route
             exact
@@ -47,7 +58,9 @@ const App = (props) => {
           <Route
             exact
             path="/new"
-            component={PostForm} 
+            render={() => (
+              <PostForm addNewPost={addNewPost} />
+            )}
           />
           <Route component={NotFound} />
         </Switch>
