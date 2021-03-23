9278e8efd1c92f2c14e963e0afc29c7df08cb81c
Kelly <lawrencek1992@gmail.com>
2021-03-23 13:39:13 -0600

    Update PostForm.js

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,55 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Redirect } from "react-router-dom";
 import Quill from "react-quill";
 
 import "react-quill/dist/quill.snow.css";
 
-const PostForm = ({ addNewPost }) => {
+const PostForm = ({ post: propsPost, addNewPost, updatePost }) => {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const [saved, setSaved] = useState(false);
-    
+    const [post, setPost] = useState({...propsPost});
+
+    const prevPostRef = useRef();
+    useEffect(() => {
+        prevPostRef.current = post;
+    }, [post]);
+    const prevPost = prevPostRef.current;
+
+    // If the user has navigated from editing a post to adding a new one, set the post state object equal to that of the new propsPost value and clear the Quill editor with the custom methods it provides [getEditor() and setContents()].
+    const quillRef = React.useRef();
+    useEffect(() => {
+        if (prevPost && quillRef.current) {
+            if (propsPost.id !== prevPost.id) {
+                setPost({ ...propsPost });
+                quillRef.current.getEditor().setContents(``);
+            }
+        }
+    }, [prevPost, propsPost]);
+
     const handleAddNewPost = (event) => {
         event.preventDefault();
-        if (!title) {
+        if (!post.title) {
             alert("Title required in order to submit post!"); 
-        } else if (!content) {
+        } else if (!post.content) {
             alert("Please write a post before submitting!");
-        } else {
-            const post = {
-                title: title,
-                content: content
-            };
-            addNewPost(post);
+        } else if (post.title) {
+            if (updatePost) {
+                updatePost(post);
+            } else {
+                addNewPost(post);
+            }
             setSaved(true);
         }
     }
     if (saved === true) {
         return <Redirect to="/" />
     }
-    
     return (
-        <form className="container">
-            <h1>Add a New Post</h1>
+        <form className="container" onSubmit={handleAddNewPost}>
+            <h1>Add New Post</h1>
             <p>
                 <label htmlFor="form-title">Title:</label>
                 <br />
                 <input
+                    defaultValue={post.title}
                     id="form-title"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
+                    value={post.title}
+                    onChange={(event) => 
+                        setPost({
+                        ...post,
+                        title: event.target.value
+                        })
+                    }
                 />
             </p>
             <p>
                 <label htmlFor="form-content">Content:</label>
             </p>
             <Quill
+                ref={quillRef}
+                defaultValue={post.content}
                 onChange={(content, delta, source, editor) => {
-                    setContent(editor.getContents() );
+                    setPost({
+                        ...post,
+                        content: editor.getContents() 
+                    });
                 }}
             />
             <p>
-                <button type="submit">Save</button>
+                <button type="submit">Submit</button>
             </p>
         </form>
     );
