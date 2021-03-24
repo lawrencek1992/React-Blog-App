6844ada892d11fc551d83572a41c6228f863829e
Kelly <lawrencek1992@gmail.com>
2021-03-23 21:39:07 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Posts from "./components/Posts";
 import Post from "./components/Post";
 import PostForm from "./components/PostForm";
 import Message from "./components/Message";
-import Login from "/components/Login";
+import Login from "/components/login";
 import NotFound from "./components/NotFound";
 
 import "./App.css";
