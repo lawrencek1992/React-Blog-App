ed2124c92415f82a34aa982b1e87e19e4506d514
Kelly <lawrencek1992@gmail.com>
2021-04-01 17:34:45 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,8 +10,7 @@ const firebaseConfig = {
     messagingSenderId: "117116205938",
     appId: "1:117116205938:web:65aa4acb952ab56db8bbeb",
     measurementId: "G-LSRJFNE3MQ"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
 export default firebase;
-firebase.analytics();
