52b009462a8a16f69fa782e0a05e57f2933214c1
Kelly <lawrencek1992@gmail.com>
2021-04-01 19:00:09 -0600

    Refactor Header.js
    Show "New Post" link if logged in, and show "Login" link if not logged in.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../context/UserContext";
 
-const Header = (props) => (
-    <header  className="App-header">
-        <ul className="container">
-            <li key="home">
-                <Link to="/">My Site</Link>
-            </li>
-            <li key="new-post">
-                <Link to="/new">New Post</Link>
-            </li>
-            <li key="login">
-                <Link to="/login">Login</Link>
-            </li>
-        </ul>
-    </header>
-);
+const Header = (props) => {
+    const { user, onLogout } = userContext(UserContext);
+
+    return (
+        <header  className="App-header">
+            <ul className="container">
+                <li key="home">
+                    <Link to="/">My Site</Link>
+                </li>
+                {user.isAuthenticated ? (
+                    <li key="new-post">
+                        <Link to="/new">New Post</Link>
+                    </li>
+                    ) : (
+                    <li key="login">
+                        <Link to="/login">Login</Link>
+                    </li>
+                )}
+            </ul>
+        </header>
+    );
+};
 
 export default Header;
