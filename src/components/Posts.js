ae7e7ebb84df862e10052f02f145e970fa57024d
Kelly <lawrencek1992@gmail.com>
2021-04-03 16:38:30 -0700

    Refactor edit and delete links
    -Import { useContext}.
    -Import UserContext.js file. 
    -Refactor edit and delete links to only be visible when the user is logged in.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,27 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState, useContext} from "react";
+import UserContext from "../context/UserContext";
 
-const Posts = ({ posts, deletePost }) => (
-    <article className="posts container">
-        <h1>Posts</h1>
-        <ul>
-            {posts.length < 1 && (
-                <li key="empty">No posts yet!</li>
-            )}
-            {posts.map(post => (
-                <li key={post.id}>
-                    <h2>
-                        <Link to={`/post/${post.slug}`}>{post.title}</Link>
-                    </h2>
-                    <p>
-                        <Link to={`/edit/${post.slug}`}>Edit</Link>
-                        {" | "}
-                        <button className="linkLike" onClick={() => deletePost(post)}>Delete</button>
-                    </p>
-                </li>
-            ))}
-        </ul>
-    </article>
-);
+const Login = (props) => {
+    const { onLogin } = useContext(UserContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-export default Posts;
+    const handleLogin = (event) => {
+        event.preventDefault();
+        onLogin(email, password);
+    };
+
+    return (
+        <form className="container" name="login" onSubmit={handleLogin}>
+            <p>
+                <label htmlFor="email">Email:</label>
+                <input type="email" onChange={(e) => setEmail(e.target.value)} />
+            </p>
+            <p>
+                <label htmlFor="password">Password:</label>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} />
+            </p>
+            <p>
+                <button type="submit" disabled={!email && !password}>Login</button>
+            </p>
+        </form>
+    );
+};
+
+export default Login;
