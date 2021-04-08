c3deccc9c2c8c1e181662abef3de2fd7fee7924b
Kelly <lawrencek1992@gmail.com>
2021-04-07 18:10:16 -0600

    Add cannotEdit and cannotDelete flash messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,9 @@ const Message = ({ type }) => {
     const messages = {
         saved: "Your post has been saved!",
         updated: "Your post has been updated!",
-        deleted: "Your post has been deleted."
+        deleted: "Your post has been deleted.",
+        cannotEdit: "Your account does not have permission to edit this post.",
+        cannotDelete: "Your account does not have permission to delete this post.",
     };
     return (
         <div className={`App-message ${type}`}>
