7d9a9aff684bccb9d7d9a07dfa901b39459183d2
Kelly <lawrencek1992@gmail.com>
2021-04-12 23:56:00 -0600

    Add date to posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,10 +4,12 @@ import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 const Post = ({ post }) => {
   const converter = new QuillDeltaToHtmlConverter(post.content.ops, {});
   const contentHTML = converter.convert();
+  const postDate = new Date(post.date).toDateString();
 
   return (
     <article className="post container">
-      <h1>{post.title}</h1>
+      <h1>{ post.title }</h1>
+      <h4>{ postDate }</h4>
       <div
         className="content"
         dangerouslySetInnerHTML={{ __html: contentHTML }}
