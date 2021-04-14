207118091e1a80ce6c71de276d1c08bbfdd0fde4
Kelly <lawrencek1992@gmail.com>
2021-04-13 20:40:52 -0600

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,8 +5,6 @@ const Message = ({ type }) => {
         saved: "Your post has been saved!",
         updated: "Your post has been updated!",
         deleted: "Your post has been deleted.",
-        cannotEdit: "Your account does not have permission to edit this post.",
-        cannotDelete: "Your account does not have permission to delete this post.",
         loggedOut: "You've been logged out.",
     };
     return (
