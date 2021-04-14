a43e54e7e08d4a8170df7c1f27edce5b0393f5e8
Kelly <lawrencek1992@gmail.com>
2021-04-13 19:32:20 -0600

    Update Header.js
    -Add FontAwesome icons for home, new post, login, and logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../context/UserContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => {
     const { user, onLogout } = useContext(UserContext);
@@ -9,27 +13,35 @@ const Header = (props) => {
         <header  className="App-header">
             <ul className="container">
                 <li key="home">
-                    <Link to="/">Home</Link>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faHome} />
+                        Home
+                    </Link>
                 </li>
-                {user.isAuthenticated ? (
-                    <>
-                        <li key="new-post">
-                            <Link to="/new">New Post</Link>
-                        </li>
-                        <li>
-                            <button
-                                className="logoutLink"
-                                onClick={(event) => {
-                                    event.preventDefault();
-                                    onLogout();
-                                }}
-                            >
+                {user.isAuthenticated && (
+                    <li key="new-post">
+                        <Link to="/new">
+                            <FontAwesomeIcon icon={faEdit} />
+                            New Post
+                        </Link>
+                    </li>
+                )}
+                {user.isAuthenticated && (
+                    <li key="logout">
+                        <button
+                            className="logoutLink"
+                            onClick={(event) => {
+                                event.preventDefault();
+                                onLogout();
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faSignOutAlt} />
                             Logout
-                            </button>
-                        </li>
-                    </>
-                    ) : (
-                    <li key="login">
+                        </button>
+                    </li>
+                )}
+                {!user.isAuthenticated && (
+                    <li key="logout">
                         <Link to="/login">Login</Link>
                     </li>
                 )}
